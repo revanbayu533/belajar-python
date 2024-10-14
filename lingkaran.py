a20803224da04c13ba994c59990c80f3674cb79f
= <=>
2024-10-14 10:01:25 +0700

    belajar python

---
+++ lingkaran.py
@@ -0,0 +1,8 @@
+def lingkaran():
+    jari_jari = int(input('masukkan jari-jari: '))
+    keliling = lambda jari_jari: 2 * 3.14 * jari_jari
+
+    print("Keliling lingkaran =",keliling(jari_jari))
+
+lingkaran()
+lingkaran()
